846ab14c3bb0e95dfe5634e7580a2ce1cbeaf943
Rishigupta02 <rishiggupta02@gmail.com>
2025-01-28 00:30:30 +0530

    server link changed

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://doctor-appointment-mern-znza.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
