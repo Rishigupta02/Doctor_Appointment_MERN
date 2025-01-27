e5c588cfb3131ae73b4f203fb876cca7980156a8
Rishigupta02 <rishiggupta02@gmail.com>
2025-01-28 00:06:55 +0530

    server link changed in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // Base URL of your backend server
+        target: 'https://doctor-appointment-mern-znza.onrender.com', // Base URL of your backend server
         changeOrigin: true, // Ensures the host header matches the target
         rewrite: (path) => path.replace(/^\/api/, '/api/v1'), // Rewrites the path
       },
